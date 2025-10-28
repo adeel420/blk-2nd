0f7ccd054281207a4d35207f7ff41dd07937c88c
Your Name <your_email@example.com>
2025-10-28 22:14:51 +0500

    Add

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  assetsInclude: ["**/*.mpeg", "**/*.mp3", "**/*.wav"],
   server: {
     proxy: {
       // All requests to /api will be proxied to your Google Apps Script
